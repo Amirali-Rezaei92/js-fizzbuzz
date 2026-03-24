39a5f9bd3ca2faee3e7f77d137743e16b5d84fcc
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-24 17:37:48 +0100

    Aggiunti commenti dettagliati per spiegare ogni parte del codice

--- script.js
+++ script.js
@@ -12,15 +12,16 @@
 //Inizio del ciclo che va da 1 a 100
 
 for (let Numero = 1; Numero <= 100; Numero++) {
-
+    // Controllo se il numero è divisibile sia per 3 che per 5
     if (Numero % 3 === 0 && Numero % 5 === 0) {
         console.log('FIZZBUZZ');}
+        // Controllo se il numero è divisibile per 5
         else if (Numero % 5 === 0) {
             console.log('BUZZ')
-        }
+        } // Controllo se il numero è divisibile per 3
         else if (Numero % 3 === 0) {
             console.log('FIZZ')
-        }
+        } // Se non è divisibile né per 3 né per 5, stampo il numero
         else {
             console.log(Numero)
         }
