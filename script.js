741eddc6ddadecd35bf89cfb55655b66d1172997
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-24 17:11:51 +0100

    Aggiunti tutti i commenti principali per spiegare la logica del programma

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 
-//inizio
+//Obiettivo:
+//Voliamo stampare i numeri 1 a 100.
+//Ogni numero divisibile per 3 deve essere stampato come "FIZZ".
+//Ogni numero divisibile per 5 deve essere stampato come "BUZZ".
+//se numero è divisibile sia per 3 che per 5, deve essere stampato "FIZZBUZZ".
+
+//=========================================================================
+//MAIN
+//==========================================================================
+
+//Inizio del ciclo che va da 1 a 100
